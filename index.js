7181f7288da74fded37ddf3c84c95e588e17b840
dangrammer <52326502+dangrammer@users.noreply.github.com>
2019-10-09 13:39:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function fetchBooks() {
-
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => {
+    renderBooks(json) 
+  })
+  return fetch('https://anapioficeandfire.com/api/books')
 }
 
 function renderBooks(json) {
@@ -14,3 +19,8 @@ function renderBooks(json) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
+
+
+
+
