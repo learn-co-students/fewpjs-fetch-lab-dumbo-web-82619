e80567ee3c131a0480065475fbeebefccb6628e2
Brett Saxon <brettcsaxon@gmail.com>
2019-10-11 12:05:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+  
+  // console.log(renderBooks(json))
 }
 
 function renderBooks(json) {
@@ -13,4 +17,5 @@ function renderBooks(json) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
+
 })
